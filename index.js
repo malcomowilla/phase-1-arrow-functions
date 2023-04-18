c6cde9b3a78cca9e81d96b3db616cb163a4826e5
malcomowilla <owillamalcom@gmail.com>
2023-04-18 19:07:03 +0300

    solved

---
+++ index.js
@@ -0,0 +1,112 @@
+const add = (parameter1, parameter2)=> parameter1 + parameter2
+
+const addNumbers = x => x + 2
+
+console.log(addNumbers(3))
+
+
+
+const sum = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2
+}
+
+
+sum(1, 2)
+
+
+const nums = [1, 2, 3];
+const squares = nums.map(x => x ** 2)
+console.log(squares)
+
+console.log(nums)
+
+
+
+
+const divide = function () {
+    return 2000 / 100
+}
+
+
+const square = x => x ** x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
